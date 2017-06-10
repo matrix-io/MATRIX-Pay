f509c7b3942addd7b16a4a48a53e798e33c2ed83
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-06-10 15:24:55 -0400

    Connect buttons in iOS start view

--- ios/MATRIX Pay/Sources/ViewController.swift
+++ ios/MATRIX Pay/Sources/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func paymentPressed(_ sender: UIButton) {
+
+    }
+
+    @IBAction func registerPressed(_ sender: UIButton) {
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
